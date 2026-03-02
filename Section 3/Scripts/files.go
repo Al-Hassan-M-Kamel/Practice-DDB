c708db5eb302190697ee8056eb02e35fbf133e2b
Al-Hassan <hkamel904@gmail.com>
2026-03-02 09:16:11 +0200

    Quiz1

--- Section 3/Scripts/files.go
+++ Section 3/Scripts/files.go
@@ -9,6 +9,42 @@ import (
 
 func main() {
 
+	in_file_path := "dataset_2_6.txt"
+	out_file_path := "output.txt"
+
+	// open file for reading...
+	file1, _ := os.OpenFile(in_file_path, os.O_RDONLY, 0)
+
+	file2, _ := os.OpenFile(out_file_path, os.O_WRONLY|os.O_CREATE, 0666)
+
+	defer file1.Close()
+	defer file2.Close()
+
+	// read the content of the first file...
+
+	bytes, _ := io.ReadAll(file1)
+	data := string(bytes)
+
+	// Write the content in the second file...
+
+	writer := bufio.NewWriter(file2)
+	writer.WriteString(data)
+
+	writer.Flush()
+}
+
+func Read_File_3() {
+	file_path := "dataset_2_6.txt"
+
+	file_handle, _ := os.Open(file_path)
+
+	defer file_handle.Close()
+
+	scanner := bufio.NewScanner(file_handle)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 }
 
 func Read_File_2() {
@@ -57,4 +93,33 @@ func Read_File_1() {
 
 		fmt.Println(line)
 	}
+}
+
+func Write_File_1() {
+	/*
+		os.OpenFile take three arguments:
+		1- file path
+		2- file mode one of (os.O_RDONLY, os.WRONLY, os.O_CREATE, os.O_TRUNC)
+		3- the file permission
+	*/
+	file_handle, err := os.OpenFile("output.txt", os.O_WRONLY|os.O_CREATE, 0666)
+
+	if err != nil {
+		fmt.Printf("An error occurred with file creation\n")
+		return
+	}
+
+	defer file_handle.Close()
+
+	// create a writer to our stream...
+	writer := bufio.NewWriter(file_handle)
+
+	output_string := "Hello World...\n"
+
+	// Here we write ten lines...
+	for i := 0; i < 10; i++ {
+		writer.WriteString(output_string)
+	}
+
+	writer.Flush() // The actual call that saves and writes all our contents and updates...
 }
